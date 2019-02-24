c0ea057293a62073714e2dbcc2c8f2de4dd76c12
Stephen De Gabrielle <spdegabrielle@gmail.com>
2019-02-24 01:49:23 +0000

    added binaries

--- info.rkt
+++ info.rkt
@@ -1,8 +1,16 @@
 #lang info
+
 (define collection "gstreamer-x86_64-macosx")
+
+(define copy-foreign-libs
+  '("libgirepository-1.0.dylib"
+    "libgobject-2.0.dylib"
+    ))
+
+
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/gstreamer-x86_64-macosx.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
+;(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+;(define scribblings '(("scribblings/gstreamer-x86_64-macosx.scrbl" ())))
+;(define pkg-desc "Description Here")
+;(define version "0.0")
 (define pkg-authors '(spdegabrielle))
