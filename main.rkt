3d7967db8abf5a7db451720a7cdb699e69f21295
Stephen De Gabrielle <spdegabrielle@gmail.com>
2019-04-26 17:26:45 +0100

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -28,24 +28,20 @@
 
 
 
+
 (module+ test
   ;; Any code in this `test` submodule runs when this file is run using DrRacket
   ;; or with `raco test`. The code here does not run when this file is
   ;; required by another module.
 
-  (check-equal? (+ 2 2) 4))
+  ;;(check-equal? (+ 2 2) 4)
+  )
 
 (module+ main
   ;; (Optional) main submodule. Put code here if you need it to be executed when
   ;; this file is run using DrRacket or the `racket` executable.  The code here
   ;; does not run when this file is required by another module. Documentation:
   ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
+  
 
-  (require racket/cmdline)
-  (define who (box "world"))
-  (command-line
-    #:program "my-program"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
+  )
